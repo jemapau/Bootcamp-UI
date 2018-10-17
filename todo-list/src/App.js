381e803c4799e59d61b63e6cb50774afe984f1ff
Maria Paulina <je.mapau@gmail.com>
2018-10-17 05:45:59 -0500

    Start TodoList project

---
+++ todo-list/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import logo from './logo.svg';
+import './main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ todo-list/src/todoList.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div className="todolist">
+                <div className="header">
+                    <form>
+                        <input placeholde="">
+                        </input>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
