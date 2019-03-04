bb251c9ab652e3347d10a64bfc513bd6c1f6dd80
Maria Paulina <je.mapau@gmail.com>
2019-03-03 20:06:15 -0500

    Update prototype

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,9 +1,10 @@
-var Persona = {
-    name: "MariaS",
-    lastName: "Arenas"
-}
+// var Person = {
+//     name: "Maria",
+//     lastName: "Arenas",
+//     gender: "Female"
+// }
 
-
+// Constructor
 function Person(name, lastName, gender) {
     this.name = name;
     this.lastName = lastName;
@@ -15,23 +16,6 @@ Person.prototype.introduce = function(){
     console.log(`Hi I'm ${this.name} ${this.lastName}`);
 }
 
-// herencia o delegacion, le esta heredando propiedades a otro objeto.
+var personOne = new Person('Mariangel', 'Arenas', 'Woman');
 
-Developer.prototype.introduceAboutJob = function() {
-    console.log(`Hi i'm ${this.name} ${this.lastName} and i have` );
-}
-
-var me4 = new Developer("andres", "soto, "h", 5);
-
-me4.introduce();
-me4.introduceAboutJob();
-
-class PersonWithClass {
-    constructor(name, lastName, gender){
-        this.name = name;
-        this.lastName = lastName;
-        this.gender = gender;
-    }
-}
-
-//metodo super, permite compartir propiedades que se comparten entre objetos
+personOne.introduce();
